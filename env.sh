dd0051f0e659bb97ae8099ac8210f64b8a182892
gatilin <gatilin@tencent.com>
2025-02-08 22:59:14 +0800

    add env.sh

---
+++ env.sh
@@ -0,0 +1,6 @@
+
+python3 -m pip install -r requirements.txt  -i https://mirrors.cloud.tencent.com/pypi/simple
+python3 -m pip install -e detectron2/ -i https://mirrors.cloud.tencent.com/pypi/simple
+python3 -m pip install -e detrex/ -i https://mirrors.cloud.tencent.com/pypi/simple
+python3 -m pip install -e CLIP/ -i https://mirrors.cloud.tencent.com/pypi/simple
+python3 -m pip install -e .
